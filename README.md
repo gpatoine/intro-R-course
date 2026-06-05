34304c197769e21f77de5a330671659497b97cd0
Guillaume Patoine <gpatoine@gmail.com>
2026-06-05 03:02:51 +0200

    better slides

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Introduction to R for Data Analysis
 
-This repository contains the materials for the course "Introduction to R", offered at IPB-Halle in June 2026.
+This repository contains slides and materials for the course "Introduction to R", offered at IPB-Halle in June 2026.
--- code/course-script.R
+++ code/course-script.R
@@ -1,3 +1,5 @@
+#created: 2026-06
+# author: Guillaume Patoine <gpatoine@ipb-halle.de>
 
 # This is a comment!
 # Because of the hash (#) symbol at the beginning.
@@ -6,12 +8,12 @@
 # You can also select a section and use ctrl + Enter to run that section
 
 
-# Let's Start ------------
+# 2. Let's Start -----------------------------------------------------------
 
-## Calculator --------------------------------------------------------------
+## R as Calculator ----
 
-# basic operations
-1 + 5
+# Basic operations
+1 + 1
 
 12 * 1.2356
 
@@ -24,46 +26,27 @@ pi / 2
 1.34e3
 
 
-# vectors
-c(2,4,6)
+# Math functions
+log(100) # natural log
 
-c(2,4,6) + 2
-
-c(3,5,8) + c(10, 11, 22)
-
-1:3
-
-1:3 * 2
-
-
-# functions
 log2(100)
 
-log(100)
-
 log10(100)
-
-exp(1)
 
 sqrt(2)
 
 
-## Variables -----------------------------------------------------
+## Variables ----
+x <- 10
+y <- 5
+x + y
 
-# assignment
+y <- 7
+y <- y + 1
+y
 
-fruit <- "apple"
 
-name <- "john"
-
-age <- 27
-age
-
-# modification
-age <- age + 1
-age
-
-# compare
+# Comparisons
 5 > 3
 5 > 5
 5 >= 5
@@ -76,144 +59,172 @@ age_mary <- 31
 age_john ___ age_mary
 
 
+## Vectors ----
+vec <- c(5,7,12)
+vec
+length(vec)
 
-## Functions ---------------------------------------------------------------
+# Operations happen component-wise!
+vec + 2
+c(3,5,8) + c(10, 11, 22)
+sqrt(vec)
 
-# random numbers
-runif(10)
-rep(1:3, 4)
-sample(letters, 5)
+# Colon creates a series of numbers
+1:3
+5:8 * 2
 
-# Function parameters can be defined by name and by position
-sample(replace = TRUE, x = 1:5, size = 10)
-sample(10, TRUE, x = 1:5)
-
-
-# Documentation
-# Put your cursor on the function and type F1
-?sample
-
-# Type F2 to see the code
-# or run the function itself without parenthesis
-sample
+# Summary functions
+mean(vec)
+sum(vec)
+max(vec)
+sd(vec)
 
 
-## Data structures ---------------------------------------------------------
-# Creation, subsetting
+## Types of variables ----
+name <- "Alice"
+age <- 25
+is_student <- TRUE
 
-class(42)
-class("apple")
+class(name)
+class(age)
+typeof(age)
+
+# TODO what do you expect?
 class(5 > 3)
 
+# Type conversion
+c(1, "2", 3)
 
-# Vector
-vec1 <- c(2,4,6)
-1:3
-1:3 * 2
+as.numeric("3")
+as.character(5)
+as.logical(1)
 
-class(vec)
-
-vec2 <- c(1,3,8,14)
-
-# using the index
-vec2[2]
-vec2[c(1,3)]
+as.numeric(c("1", "2", "a"))
 
 
+# Missing values
+nums <- c(3,5,NA)
+mean(nums)
+sqrt(-2)
+
+mean(nums, na.rm = TRUE)
+
+
+
+## Data structures in R ----
+
+# Vector indexing
+vec <- c(4,6,2,11)
+vec[2]
+vec[c(1,4)]
+
+vec[-2]
+vec[-c(1,4)]
+
+vec[vec > 3]
 
 # TODO make a vector of three fruits
 fruits <- c("apple", ___)
-
 length(fruits)
 
 
-# factors
-animals <- c("cat", "dog", "dog", "bird", "cat", "cat")
-animals
+# Matrices and arrays
+mat <- matrix(1:6, nrow = 2)
+mat
+class(mat)
 
-ani_fact <- factor(animals)
-ani_fact
-
-
-# Matrix (Array)
-
-nums <- runif(24)
-
-mat <- matrix(nums, nrow = 6, ncol = 4)
-
-# subsetting uses mat[ROWS, COLUMNS]
-mat[1,1]
-mat[c(2,3), 2]
-# leave it empty to use all
+mat[1,3]
 mat[1,]
 
-arr <- array(nums, c(3, 4, 2))
-arr[1,1,1]
 
+# Lists
+lst <- list(
+  name = "Alice",
+  age = 25,
+  scores = c(10, 20, 30),
+  fav_colors = c("purple", "turquoise")
+)
 
-# List
+lst
 
-mylist <- list(animals, nums, mat, arr)
+# Use [] to subset a list
+# Use [[]] to take elements out
+mylist <- list(1:3, "a", 4:6)
 class(mylist[1])
 class(mylist[[1]])
 
 
+# Data frames and tibbles
+df <- data.frame(
+  name = c("Alice", "Bob", "Josh"),
+  age = c(25, 30, 20),
+  wears_glasses = c(TRUE, FALSE, TRUE)
+)
 
+df
 
-# Dataframe (Tibble)
-# A dataframe is a list where the elements have the same length
-
-
-
-
-
-## subsetting --------------------------------------------------------------
-
-
-## Dealing with errors -----------------------------------------------------
-
-sample(1:5, size = 10)
-
-# TODO fix the function by specifying another parameter
-sample(1:5, size = 10, ___)
+df$age
+df[1, ]
+df[, "name"]
+df[df$age > 21,]
+df[!df$wears_glasses,]
 
 
 
-## Packages ----------------------------------------------------------------
+## Functions ----
 
-# Let's check our R version first
-# We can compare online to see how recent it is
+seq(from = 2, to = 8)
+
+# Writing a function
+add_one <- function(x) {
+  x + 1
+}
+add_one(10)
+
+# Documentation
+# Also try F1 and F2 on the function name
+?seq
+sample
+
+# Arguments
+seq(2,11,3)
+seq(by = 3, from = 2, to = 11)
+seq(to = 11, 3, from = 2)
+
+
+
+# 3. A data analysis workflow -----------------------------------------------------
+
+## Setup ----
+
+getwd()
 version
 
-# We can install from CRAN
-install.packages("dplyr")
+
+## Packages ----
+
 install.packages("tidyverse")
-
-# or BioConductor
-install.packages("BiocManager")
-BiocManager::install("ComplexHeatmap")
-
-
-# It is also possible to install directly from github
-install.packages("remotes")
-remotes::install_github("ipb-halle/MetFamily")
-
-# To use a package, we need to attach (load) it
-library(dplyr)
-
-
-filter
-
-
-# Example workflow --------------------------------------------------------
-
-# Welcome to the tidyverse 
-# includes readr, tibble, dplyr, ggplot2, stringr
 library(tidyverse)
 
+# TODO read the messages, what does it mean?
 
 
-## Reading data ------------------------------------------------------------
+# The %>% pipe
+1:10 %>%
+  mean()
+
+mean(1:10)
+
+1:10 %>%
+  sqrt() %>%
+  mean()
+
+
+
+
+# HERE
+
+## Reading data ----
 library(readr)
 library(readxl) # did it work?
 
@@ -226,13 +237,33 @@ read_xlsx
 
 
 
-## Wrangling with dplyr ----------------------------------------------------
+## Wrangling with dplyr ----
 library(dplyr)
 
 
+## Side note: Regular Expressions
+
+library(stringr)
+
+fruits <- c("apple", "banana", "pineapple", "orange")
+
+str_detect(fruits, "appl")
+
+# which ones start with a "p"
+str_detect(fruits, "^p")
+
+# which ones start with a vowel?
+str_detect(fruits, "^[aeiou]")
+
+# TODO which ones have a vowel after a "n"
+str_detect(fruits, "___")
+
+str_remove(fruits, "a")
+str_remove_all(fruits, "a")
 
 
-## Saving data -------------------------------------------------------------
+
+## Saving data ----
 
 saveRDS
 
@@ -254,31 +285,60 @@ write_xl
 
 
 
-## Side note: Regular Expressions ------------------------------------------
-
-library(stringr)
-
-fruits <- c("apple", "banana", "pineapple", "orange")
-
-str_detect(fruits, "appl")
-
-# which ones start with a "p"
-str_detect(fruits, "^p")
-
-# which ones start with a vowel?
-str_detect(fruits, "^[aeiou]")
-
-# TODO which ones have a vowel after a "n"
-str_detect(fruits, "___")
-
-str_remove(fruits, "a")
-str_remove_all(fruits, "a")
 
 
-## Figures with ggplot2 ----------------------------------------------------
+
+## Figures with ggplot2 ----
 library(ggplot2)
 
 
+# Load metabolic dataset
+
+# PCA
+
+# Heatmap
 
 
 
+
+
+
+
+
+# Bonus -------------------------------------------------------------------
+
+# factors
+animals <- c("cat", "dog", "dog", "bird", "cat", "cat")
+animals
+
+# factors have levels, and are encoded as numbers
+ani_fact <- factor(animals)
+ani_fact
+as.numeric(ani_fact)
+
+
+# Dealing with errors
+sample(1:5, size = 10)
+
+# TODO fix the function by specifying another parameter
+sample(1:5, size = 10, ___)
+
+
+# Some common functions used. Look up what they do.
+runif(10)
+rep(1:3, 4)
+sample(letters, 5)
+
+
+# What does the following function do?
+mystery_fct <- function(number) {
+  
+  if (number > 0) {
+    return("positive")
+  } else if (number < 0) {
+    return("negative")
+  } else {
+    return("zero")
+  }
+  
+}
